01e97ea1fc914d058ffca928cb795e16bb349cf4
Matrix Website <matrix@website.com>
2025-10-30 14:38:31 +0300

    Update configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/piontech_website',
   assetPrefix: '/piontech_website/',
+  output: 'export',
 };
 
 export default nextConfig;
